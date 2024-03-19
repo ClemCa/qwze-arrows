859d052b7ef2631c769a05c62534a0a01dae6e52
ClemCa <perso.catorc@gmail.com>
2024-03-19 19:53:58 -0400

    working extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/* eslint-disable curly */
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "qwze-arrows" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('qwze-arrows.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from qwze arrows!');
+	let toggleCommand = vscode.commands.registerCommand('qwze-arrows.toggle', () => {
+		if(vscode.workspace.getConfiguration('qwze-arrows').get('enabled')) {
+			vscode.workspace.getConfiguration('qwze-arrows').update('enabled', false, true);
+			vscode.window.showInformationMessage('Arrows insertion disabled!');
+			return;
+		}
+		vscode.workspace.getConfiguration('qwze-arrows').update('enabled', true, true);
+		vscode.window.showInformationMessage('Arrows insertion enabled!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(toggleCommand);
+
+	vscode.workspace.onDidChangeTextDocument(arrowDetection);
 }
 
-// This method is called when your extension is deactivated
+function arrowDetection(change: vscode.TextDocumentChangeEvent) {
+	if(change.reason !== undefined) return;
+	if(!vscode.workspace.getConfiguration('qwze-arrows').get('enabled')) return;
+	function Check(text: string) {
+		if(text.length < 2) return;
+		if(text[text.length-1] === 'w' && text[text.length-2] === 'w') arrowInsertion('<');
+		if(text[text.length-1] === 'x' && text[text.length-2] === 'x') arrowInsertion('>');
+	}
+	if(change.contentChanges.length === 0) return;
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+	const cursor = editor.selection.active;
+	const changedBefore = editor.document.lineAt(cursor.line).text.slice(cursor.character-1, cursor.character);
+	const changedAfter = change.contentChanges[change.contentChanges.length-1].text;
+	// vscode gives us the last key pressed, and does not update the cursor position
+	const changedText = changedBefore + changedAfter;
+	Check(changedText);
+}
+
+function arrowInsertion(arrow: '<' | '>') {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+	editor.edit(editBuilder => {
+		const cursor = editor.selection.active;
+		editBuilder.replace(new vscode.Range(cursor.line, cursor.character - 1, cursor.line, cursor.character+1), arrow);
+	}, {undoStopBefore: true, undoStopAfter: true});
+	editor.selection.active = new vscode.Position(editor.selection.active.line, editor.selection.active.character-1);
+}
+
 export function deactivate() {}
