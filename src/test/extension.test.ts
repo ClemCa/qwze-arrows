043ab0f5335baf9c6a7b392369dbb2309d780754
ClemCa <perso.catorc@gmail.com>
2024-03-19 20:22:33 -0400

    Add tests for toggling & insertion

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -8,8 +8,59 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Toggle feature test', async () => {
+		const enabled = vscode.workspace.getConfiguration('qwze-arrows').get('enabled');
+
+		await vscode.commands.executeCommand('qwze-arrows.toggle');
+		assert.notStrictEqual(enabled, vscode.workspace.getConfiguration('qwze-arrows').get('enabled'));
+
+		await vscode.commands.executeCommand('qwze-arrows.toggle');
+		assert.strictEqual(enabled, vscode.workspace.getConfiguration('qwze-arrows').get('enabled'));
+	});
+
+	test('W-only toggle test', async () => {
+		const wOnly = vscode.workspace.getConfiguration('qwze-arrows').get('wOnly');
+
+		await vscode.commands.executeCommand('qwze-arrows.wOnlyToggle');
+		assert.strictEqual(wOnly, vscode.workspace.getConfiguration('qwze-arrows').get('wOnly'));
+
+		await vscode.commands.executeCommand('qwze-arrows.wOnlyToggle');
+		assert.strictEqual(wOnly, vscode.workspace.getConfiguration('qwze-arrows').get('wOnly'));
+	});
+
+	test('Arrow insertion test', async () => {
+		await vscode.workspace.getConfiguration('qwze-arrows').update('enabled', true, true);
+		await vscode.workspace.getConfiguration('qwze-arrows').update('wOnly', false, true);
+
+		const testDocument = await vscode.workspace.openTextDocument({ content: 'xx', language: 'plaintext' });
+		const testEditor = await vscode.window.showTextDocument(testDocument);
+
+		const position = new vscode.Position(0, 0);
+		testEditor.selection = new vscode.Selection(position, position);
+
+		// base x test
+		await testEditor.edit(editBuilder => {
+			editBuilder.insert(position, 'x');
+		});
+		assert.strictEqual(testEditor.document.getText(), 'x>');
+
+		// base w test
+		await testEditor.edit(editBuilder => {
+			editBuilder.insert(position, 'w');
+		});
+		await testEditor.edit(editBuilder => {
+			editBuilder.insert(position, 'w');
+		});
+		assert.strictEqual(testEditor.document.getText(), 'x><');
+
+		// wOnly test
+		await vscode.workspace.getConfiguration('qwze-arrows').update('wOnly', true, true);
+		await testEditor.edit(editBuilder => {
+			editBuilder.insert(position, 'W');
+		});
+		await testEditor.edit(editBuilder => {
+			editBuilder.insert(position, 'W');
+		});
+		assert.strictEqual(testEditor.document.getText(), 'x><>');
 	});
 });
